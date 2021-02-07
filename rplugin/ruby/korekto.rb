134ee533512de6cb1e7fe3645760db74c25da083
carlosjhr64 <carlosjhr64@gmail.com>
2021-02-07 14:52:09 -0800

    move cursor to first error

--- rplugin/ruby/korekto.rb
+++ rplugin/ruby/korekto.rb
@@ -2,6 +2,7 @@ Neovim.plugin do |plug|
   plug.command(:Korekto) do |nvim|
     validations = nvim.command_output('w !korekto').strip.split("\n").map(&:strip)
     unless validations.empty?
+      move = true
       buf = nvim.get_current_buf
       while validation = validations.shift
         validation = validation.split(':',6)
@@ -9,14 +10,20 @@ Neovim.plugin do |plug|
         if n > 0
           line = buf[n].split('#',2).first.rstrip
           line << "\t#"
-          line << validation[3] unless validation[3].empty?
+          unless validation[3].empty?
+            line << validation[3]
+            if move # to first error
+              move = false
+              nvim.get_current_win.set_cursor([n,0])
+            end
+          end
           line << ' '
           line << validation[4]
           unless validation[5].empty?
             line << ': '
             line << validation[5]
           end
-          buf[n] = line
+          buf[n] = line unless line == buf[n]
         end
       end
     end
