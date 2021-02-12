779f01d2382e01f46dc8576a429fecfc33d9e9b3
carlosjhr64 <carlosjhr64@gmail.com>
2021-02-12 04:05:24 -0800

    korekto/symbols

--- lib/korekto.rb
+++ lib/korekto.rb
@@ -1,57 +1,11 @@
+require 'korekto/symbols'
+
 class Korekto
   VERSION = '0.0.210212'
 
   class KorektoError < Exception
   end
 
-  class Symbols
-    def to_a
-      @a
-    end
-
-    def initialize
-      @a = []
-    end
-
-    def insert(w)
-      if index = @a.bsearch_index{|c| c >= w}
-        @a.insert(index, w)  unless w == @a.fetch(index)
-      else
-        @a.push w
-      end
-    end
-
-    def undefined(statement)
-      undefined,chars = [],statement.chars
-      while w = chars.shift
-        if w==':'
-          while c = chars.shift
-            break(chars.unshift c) if c=~/\W/
-            w<<c
-          end
-        end
-        undefined.push w unless @a.bsearch{|c| w <=> c}
-      end
-      return undefined
-    end
-
-    def defined?(w)
-      @a.bsearch{|c| w <=> c} ? true : false
-    end
-
-    def define!(statement)
-      chars = statement.chars
-      while w = chars.shift
-        if w==':'
-          while c = chars.shift
-            break(chars.unshift c) if c=~/\W/
-            w<<c
-          end
-        end
-        insert(w)
-      end
-    end
-  end
   SYMBOLS = Symbols.new
 
   SYNTAX = []
