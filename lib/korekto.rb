1df4880c86b189ad712539db39d8cf36400e7a32
carlosjhr64 <carlosjhr64@gmail.com>
2021-02-12 13:02:50 -0800

    lot of restructuring

--- lib/korekto.rb
+++ lib/korekto.rb
@@ -1,46 +1,23 @@
 require 'korekto/symbols'
-require 'korekto/statements'
 require 'korekto/syntax'
 require 'korekto/statement_to_regexp'
 require 'korekto/heap'
+require 'korekto/statements'
 
 class Korekto
   VERSION = '0.0.210212'
 
-  class KorektoError < Exception
+  def initialize(filename='-')
+    @filename = filename
+    @line,@active = nil,false
   end
 
-  SYMBOLS    = Symbols.new
-  SYNTAX     = Syntax.new
-  STATEMENTS = Statements.new
-  S2R        = StatementToRegexp.new
-  HEAP       = Heap.new(13)
-
+=begin
   # TODO:
   LOGIC = {}
   TYPE = {}
   PATTERN = {}
 
-  def initialize(filename='-')
-    @filename = filename
-    @active = false
-    @line_number = 0
-    @statement = @code = @title = nil
-  end
-
-  def active?
-    case @line
-    when /^```korekto$/
-      raise 'unexpected fence' if @active
-      @active = true
-      false
-    when /^```\s*$/
-      @active = false
-      false
-    else
-      @active
-    end
-  end
 
   # TODO:
   def pattern(name, pattern)
@@ -58,184 +35,75 @@ class Korekto
       TYPE[variable] = name
     end
   end
+=end
 
-  def statement?
+  def active?
     case @line
-    when /^\s*#/ # comment
+    when /^```korekto$/
+      raise 'unexpected fence' if @active
+      @active = true
       false
-    when %r{^< ([/\w\-.]+)$} # import
-      filename = $1.strip
-      Korekto.new(filename).run
+    when /^```\s*$/
+      @active = false
       false
-    when /^(::[A-Z]\w+)#(\w+)([^=]*=.+)$/ # patch
-      klass,method,definition = $1,$2,$3
-      raise "overides: #{klass}##{method}" if eval(klass).method_defined? method
-      eval <<~EVAL
-        class #{klass}
-          def #{method}#{definition}
-        end
-      EVAL
-      false
+    else
+      @active and not (/^\s*#/).match?(@line)
+    end
+  end
+
+  def patch(klass, method, definition)
+    raise "overides: #{klass}##{method}" if eval(klass).method_defined? method
+    eval <<~EVAL
+      class #{klass}
+        def #{method}#{definition}
+      end
+    EVAL
+  end
+
+  SYNTAX = Syntax.new
+  def preprocess?
+    case @line
+    when %r{^< (?<filename>[/\w\-.]+)$}
+      Korekto.new($~[:filename].strip).run
+    when /^(?<klass>::[A-Z]\w+)#(?<method>\w+)(?<definition>[^=]*=.+)$/ # patch
+      patch($~[:klass],$~[:method],$~[:definition])
     when /^[?] (\w.*)$/
       SYNTAX.push $1.strip
-      false
+=begin
     when %r{^! (\p{L}|:\w+)/(.*)/$}
       # TODO:
       pattern($1, $2)
     when /^! (\p{L}|:\w+){\p{L}( \p{L})*}$/
       # TODO:
       types($1, $2.split)
-    when %r{^(?<statement>.*)\s#(?<code>[A-Z](\d+(/[A-Z]\d+(,[A-Z]\d+)*)?)?)( (?<title>[A-Z][\w\s]*\w))?$}
-      # Valid statements must end with commentary like #X101:Y12,Z80  Statement title
-      @statement,@code,@title = $~[:statement].strip,$~[:code],$~[:title]
-      true
+=end
     else
-      raise 'unrecognized korekto line'
+      return false
     end
+    true
   end
 
-  def syntax_checker
-    SYNTAX.each do |rule|
-      raise KorektoError, "syntax: #{rule}" unless @statement.instance_eval(rule)
-    rescue
-      raise "#{$!.class}: #{rule}"
-    end
-  end
-
-  def set_statement(code, support=nil, title=nil)
-    n = STATEMENTS.length + 1
-    @code = "#{code}#{n}"
-    @code += "/#{support}" if support
-    @title = title if title and not @title
-    code_title = (@title)? "#{@code} #{@title}" : @code
-    STATEMENTS[@statement] = code_title
-  end
-
-  def all_defined
-    unless (u = SYMBOLS.undefined(@statement)).empty?
-      raise KorektoError, "undefined: #{u.join(' ')}"
-    end
-  end
-
-  def assert_not_provable
-    raise KorektoError, 'tautology' if tautology?
-    raise KorektoError, 'inferable' if inferable?
-  end
-
-  def definition
-    raise KorektoError, 'nothing was undefined' if SYMBOLS.undefined(@statement).empty?
-    assert_not_provable unless OPTIONS.fast?
-    SYMBOLS.define! @statement
-    set_statement('D')
-  end
-
-  def postulate
-    all_defined
-    assert_not_provable unless OPTIONS.fast?
-    set_statement('P')
-  end
-
-  def axiom
-    S2R[@statement] # memoize/register
-    @heap = false # Axioms are statements about single statements
-    set_statement('A')
-  end
-
-  def tautology?
-    STATEMENTS.type('A').any?{|statement, code| S2R[statement].match? @statement}
-  end
-
-  def tautology
-    all_defined
-    axiom, code = STATEMENTS.type('A').detect do |statement, code|
-      S2R[statement].match? @statement
-    end
-    raise KorektoError, "does not match any axiom" unless axiom
-    support,title = code.split(' ', 2)
-    support,_ = support.split('/', 2)
-    set_statement('T', support, title)
-  end
-
-  def inference
-    S2R[@statement]
-    @heap = false # Inference are statements about compound statements
-    set_statement('I')
-  end
-
-  def inferable?
-    mapping,*_ = infer
-    not mapping.nil?
-  end
-
-  def infer
-    mapping = nil
-    s1,s2,s3=nil,nil,@statement
-    HEAP.combos do |s1, s2|
-      compound = [s1,s2,s3].join("\n")
-      mapping, code = STATEMENTS.type('I').detect do |statement, code|
-        S2R[statement].match? compound
-      end
-      break if mapping
-    end
-    return mapping, s1, s2
-  end
-
-  def conclusion
-    all_defined
-    mapping,s1,s2 = infer
-    raise KorektoError, "does not match any inference" unless mapping
-    cm,title = STATEMENTS[mapping].split(' ',2)
-    c1,_ = STATEMENTS[s1].split(' ',2); c1,_ = c1.split('/', 2)
-    c2,_ = STATEMENTS[s2].split(' ',2); c2,_ = c2.split('/', 2)
-    support = [cm,c1,c2].join(',')
-    set_statement('C', support, title)
-  end
-
-  def restatement
-    code_title = STATEMENTS[@statement]
-    @code,title = code_title.split(' ',2)
-    # Use the restatement commentary if given:
-    @title=title unless @title
-  end
-
-  def set_acceptance_code
-    @heap = true
-    if STATEMENTS.key? @statement
-      restatement
-    else
-      case @code[0]
-      when 'D'
-        definition
-      when 'P'
-        postulate
-      when 'A'
-        axiom
-      when 'T'
-        tautology
-      when 'I'
-        inference
-      when 'C'
-        conclusion
-      else
-        raise "Statement type #{@code} not supported."
-      end
-    end
-    HEAP.add @statement if @heap
-  end
-
+  STATEMENTS = Statements.new
+  FORMAT = %r{^(?<statement>.*)\s#(?<code>[A-Z](\d+(/[A-Z]\d+(,[A-Z]\d+)*)?)?)( (?<title>[A-Z][\w\s]*\w))?$}
   def parse(lines)
+    line_number = 0
     while @line = lines.shift
       begin
-        @line_number += 1
+        line_number += 1
         next unless active?
-        next unless statement?
-        syntax_checker
-        set_acceptance_code
-        puts "#{@filename}:#{@line_number}:0:#{@code}:#{@title}"
-      rescue KorektoError
-        puts "#{@filename}:#{@line_number}:0:!:#{$!.message}"
+        next if preprocess?
+        if md = FORMAT.match(@line)
+          statement,code,title = md[:statement].strip,md[:code],md[:title]
+          code,title = STATEMENTS.add(statement, code, title)
+          puts "#{@filename}:#{line_number}:0:#{code}:#{title}"
+        else
+          raise 'unrecognized korekto line'
+        end
+      rescue Statements::Error
+        puts "#{@filename}:#{line_number}:0:!:#{$!.message}"
       rescue
-        puts "#{@filename}:#{@line_number}:0:?:#{$!.message}"
+        puts "#{@filename}:#{line_number}:0:?:#{$!.message}"
+        $stderr.puts $!.backtrace if $VERBOSE
         exit
       end
     end
--- lib/korekto/statements.rb
+++ lib/korekto/statements.rb
@@ -1,7 +1,15 @@
 class Korekto
   class Statements
+    class Error < RuntimeError; end
+
+    SYMBOLS = Symbols.new
+    S2R     = StatementToRegexp.new
+    HEAP    = Heap.new(13)
+
+    attr_reader :statement,:code,:title
     def initialize
       @h = {}
+      @statement = @code = @title = nil
     end
 
     def to_h
@@ -16,16 +24,139 @@ class Korekto
       @h.length
     end
 
-    def [](s)
-      @h[s]
+    def key?(statement)
+      @h.key?(statement)
     end
 
-    def []=(s,c)
-      @h[s]=c
+    def add(statement, code, title)
+      if code_title = @h[statement]
+        # Restatement
+        code,_ = code_title.split(' ', 2)
+        title ||=_
+        return code, title
+      end
+      @statement,@code,@title = statement,code,title
+      syntax_check
+      @heap = true
+      set_acceptance_code
+      HEAP.add @statement if @heap
+      return @code, @title
     end
 
-    def key?(s)
-      @h.key?(s)
+    private
+
+    def set_statement(code, support=nil, title=nil)
+      @code = "#{code}#{ @h.length + 1 }"
+      @code += "/#{support}" if support
+      @title = title if title and not @title
+      @h[@statement] = (@title)? "#{@code} #{@title}" : @code
+    end
+
+    def syntax_check
+      SYNTAX.each do |rule|
+        raise Error, "syntax: #{rule}" unless @statement.instance_eval(rule)
+      rescue
+        raise "#{$!.class}: #{rule}"
+      end
+    end
+
+    def set_acceptance_code
+      case @code[0]
+      when 'D'
+        definition
+      when 'P'
+        postulate
+      when 'A'
+        axiom
+      when 'T'
+        tautology
+      when 'I'
+        inference
+      when 'C'
+        conclusion
+      else
+        raise "Statement type #{@code} not supported."
+      end
+    end
+
+=begin
+    # TODO: not sure I want this
+    def tautology?
+      type('A').any?{|statement, code| S2R[statement].match? @statement}
+    end
+
+    def inferable?
+      mapping,*_ = infer
+      not mapping.nil?
+    end
+
+    def assert_not_provable
+      raise Error, 'tautology' if tautology?
+      raise Error, 'inferable' if inferable?
+    end
+=end
+
+    def definition
+      raise Error, 'nothing was undefined' if SYMBOLS.undefined(@statement).empty?
+#     assert_not_provable unless OPTIONS.fast?
+      SYMBOLS.define! @statement
+      set_statement('D')
+    end
+
+    def all_defined
+      unless (u = SYMBOLS.undefined(@statement)).empty?
+        raise Error, "undefined: #{u.join(' ')}"
+      end
+    end
+
+    def postulate
+      all_defined
+#     assert_not_provable unless OPTIONS.fast?
+      set_statement('P')
+    end
+
+    def axiom
+      S2R[@statement] # memoize/register
+      @heap = false # Axioms are statements about single statements
+      set_statement('A')
+    end
+
+    def tautology
+      all_defined
+      axiom, code = type('A').detect do |statement, code|
+        S2R[statement].match? @statement
+      end
+      raise Error, "does not match any axiom" unless axiom
+      support,title = code.split(' ', 2)
+      support,_ = support.split('/', 2)
+      set_statement('T', support, title)
+    end
+
+    def inference
+      S2R[@statement]
+      @heap = false # Inference are statements about compound statements
+      set_statement('I')
+    end
+
+    def conclusion
+      all_defined
+      inference,s1,s2 = infer
+      raise Error, "does not match any inference" unless inference
+      cm,title = @h[inference].split(' ',2)
+      c1,_ = @h[s1].split(' ',2); c1,_ = c1.split('/', 2)
+      c2,_ = @h[s2].split(' ',2); c2,_ = c2.split('/', 2)
+      support = [cm,c1,c2].join(',')
+      set_statement('C', support, title)
+    end
+
+    def infer
+      HEAP.combos do |s1, s2|
+        compound = [s1,s2,@statement].join("\n")
+        type('I').each do |inference, code|
+          return inference,s1,s2 if S2R[inference].match?(compound)
+        end
+      end
+      return nil
     end
   end
 end
