f1b480224fbf21d1b4f58d08202472a8d53665bf
carlosjhr64 <carlosjhr64@gmail.com>
2021-02-14 07:06:01 -0800

    test for heap, s2r, symbols,and syntax.

--- lib/korekto.rb
+++ lib/korekto.rb
@@ -2,7 +2,7 @@ module Korekto
   VERSION = '0.0.210214'
   require 'korekto/symbols'
   require 'korekto/syntax'
-  require 'korekto/statement_to_regexp'
+  require 'korekto/s2r'
   require 'korekto/heap'
   require 'korekto/statement'
   require 'korekto/statements'
