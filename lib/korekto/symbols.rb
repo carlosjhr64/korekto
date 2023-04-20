ccbb886b67eb1c5fe79b720204569da1d3a97b25
CarlosJHR64 <carlosjhr64@gmail.com>
2023-04-20 13:30:29 -0700

    rubocop

--- lib/korekto/symbols.rb
+++ lib/korekto/symbols.rb
@@ -1,6 +1,7 @@
 module Korekto
 class Symbols
   attr_reader :t2p, :v2t
+
   def initialize
     @h   = {}
     @t2p = {}
@@ -8,24 +9,30 @@ class Symbols
     @scanner = /:\w+|./
   end
 
+  # rubocop: disable Naming/AccessorMethodName
   def set_scanner(value) = @scanner=Regexp.new(value)
+  # rubocop: enable Naming/AccessorMethodName
 
   def undefined(statement)
     undefined = []
     if statement.pattern?
       unless statement.literal_regexp?
-        statement.scan(@scanner){|w| undefined.push(w) unless @v2t.include?(w) or @h.include?(w)}
+        statement.scan(@scanner) do |w|
+          undefined.push(w) unless @v2t.include?(w) || @h.include?(w)
+        end
       end
     else
       statement.scan(@scanner){|w| undefined.push(w) unless @h.include?(w)}
     end
-    return undefined.uniq
+    undefined.uniq
   end
 
   def define!(statement)
     if statement.pattern?
       unless statement.literal_regexp?
-        statement.scan(@scanner){|w| @h[w]=nil unless @v2t.include?(w) or @h.include?(w)}
+        statement.scan(@scanner) do |w|
+          @h[w]=nil unless @v2t.include?(w) || @h.include?(w)
+        end
       end
     else
       statement.scan(@scanner){|w| @h[w]=nil unless @h.include?(w)}
@@ -33,14 +40,14 @@ class Symbols
   end
 
   def s2r(statement)
-    if statement[0]=='/' and statement[-1]=='/'
+    if statement[0]=='/' && statement[-1]=='/'
       Regexp.new(statement[1..-2])
     else
       pattern,count,seen = '\A',0,{}
       statement.scan(@scanner) do |v|
-        if n=seen[v]
+        if (n=seen[v])
           pattern << '\\'+n
-        elsif type = @v2t[v]
+        elsif (type = @v2t[v])
           regex = @t2p[type]
           if type[0]=='.'
             pattern << regex
@@ -52,7 +59,8 @@ class Symbols
         else
           # Escape Regexp specials
           v = Regexp.quote v
-          # To avoid collisions with back-references, isolate digit in square brackets:
+          # To avoid collisions with back-references,
+          # isolate digit in square brackets:
           '0123456789'.include?(_=v[0]) and v[0]='['+_+']'
           pattern << v
         end
