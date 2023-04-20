14e8264c03cb42f87646739604fe2f25aa22c01b
CarlosJHR64 <carlosjhr64@gmail.com>
2023-04-20 13:49:17 -0700

    rubocop

--- rplugin/ruby/korekto.rb
+++ rplugin/ruby/korekto.rb
@@ -3,28 +3,28 @@ VERSION = `#{KOREKTO} --version`.strip
 
 Neovim.plugin do |plug|
   plug.command(:Korekto) do |nvim|
-    validations = nvim.command_output("w !#{KOREKTO} --edits").strip.split("\n").map(&:strip)
+    validations = nvim.command_output("w !#{KOREKTO} --edits")
+                  .strip.split("\n").map(&:strip)
     msg = "Korekto! #{VERSION}"
     unless validations.empty?
       buf = nvim.get_current_buf
-      while validation = validations.shift
+      while (validation = validations.shift)
         fields = validation.split(':',4)
         n = fields[1].to_i
-        if n > 0
-          fn,ln,code,title = fields
-          if code=='?' or code=='!'
-            # move onto error if on page
-            nvim.get_current_win.set_cursor([n,0]) if fn=='-'
-            # echo error message
-            msg = "#{fn}:#{ln}: #{title}"
-            # and stop.
-            break
-          elsif fn=='-'
-            line = buf[n].sub(/#.*$/,'').rstrip
-            line << "\t##{code}"
-            line << " #{title}" unless title==''
-            buf[n] = line unless line == buf[n]
-          end
+        next unless n.positive?
+        fn,ln,code,title = fields
+        if %w[? !].include?(code)
+          # move onto error if on page
+          nvim.get_current_win.set_cursor([n,0]) if fn=='-'
+          # echo error message
+          msg = "#{fn}:#{ln}: #{title}"
+          # and stop.
+          break
+        elsif fn=='-'
+          line = buf[n].sub(/#.*$/,'').rstrip
+          line << "\t##{code}"
+          line << " #{title}" unless title==''
+          buf[n] = line unless line == buf[n]
         end
       end
     end
