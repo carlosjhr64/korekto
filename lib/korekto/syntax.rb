8faa5b74e61a59af49e86595e86f54ff525f0f4e
CarlosJHR64 <carlosjhr64@gmail.com>
2023-04-20 13:40:31 -0700

    rubocop

--- lib/korekto/syntax.rb
+++ lib/korekto/syntax.rb
@@ -1,12 +1,14 @@
 module Korekto
 class Syntax
   def initialize = @a=[]
-  def each = @a.each{|s| yield s}
+  def each(&blk) = @a.each{|s| blk[s]}
 
   def push(s)
     # ensure it'll eval on string and returns boolean
     b = ''.instance_eval(s)
+    # rubocop: disable Style/DoubleNegation
     raise Error, 'syntax rule must eval boolean' unless b==!!b
+    # rubocop: enable Style/DoubleNegation
     @a.push(s)
   rescue
     raise Error, "#{$!.class}: #{s}"
