ba8221f1edfcf674ee75a5395c838224edcbf062
CarlosJHR64 <carlosjhr64@gmail.com>
2023-12-22 09:40:23 -0800

    Central VERSION

--- lib/korekto.rb
+++ lib/korekto.rb
@@ -1,14 +1,18 @@
 module Korekto
   VERSION = '1.6.231222'
   class Error < Exception; end
-  require 'korekto/symbols'
-  require 'korekto/syntax'
-  require 'korekto/heap'
-  require 'korekto/statement'
-  require 'korekto/statements'
-  require 'korekto/main'
-  def self.run = Korekto::Main.new.run
+
+  def self.run
+    require 'korekto/symbols'
+    require 'korekto/syntax'
+    require 'korekto/heap'
+    require 'korekto/statement'
+    require 'korekto/statements'
+    require 'korekto/main'
+    Korekto::Main.new.run
+  end
 end
+
 # Requires:
 # `ruby`
 # `nvim`
