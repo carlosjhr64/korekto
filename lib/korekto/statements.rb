b5ce664695eda7d4585ddaec200b40b36958beff
CarlosJHR64 <carlosjhr64@gmail.com>
2025-12-04 11:48:38 -0800

    Korekto::Statments rubocop autocorrect

--- lib/korekto/statements.rb
+++ lib/korekto/statements.rb
@@ -1,46 +1,52 @@
+# frozen_string_literal: true
+
 module Korekto
-class Statements
-  attr_reader :heap,:symbols,:syntax,:handwaves,:last
+  class Statements
+    attr_reader :heap, :symbols, :syntax, :handwaves, :last
 
-  def initialize
-    @statements = {}
-    @heap = Heap.new Korekto.heap
-    @symbols = Symbols.new
-    @syntax = Syntax.new
-    @handwaves = Handwaves.new(self)
-    @last = nil
-  end
-
-  def type(c)  = @statements.values.select{_1.type==c}
-  def length   = @statements.length
-  def patterns = @statements.values.select(&:pattern?).each{yield _1}
-  def get(key) = @statements[key]
-
-  def add(statement,code,title,filename)
-    c = code[0]; w = c=='W'
-    @syntax.check(statement) unless statement[0]=='/' &&
-                                    statement[-1]=='/' &&
-                                    %w[A L M E I].include?(c)
-    restatement = @statements.values.detect do |restatement|
-      (w || restatement.type==c) && restatement.to_s==statement
+    def initialize
+      @statements = {}
+      @heap = Heap.new Korekto.heap
+      @symbols = Symbols.new
+      @syntax = Syntax.new
+      @handwaves = Handwaves.new(self)
+      @last = nil
     end
-    if restatement
-      unless 'DXSPTCRH'.include?(restatement.type)
-        # Only allow heap-able statements to be restated.
-        raise Error, "restatement: #{restatement.code}"
+
+    def type(c)  = @statements.values.select { it.type == c }
+    def length   = @statements.length
+    def patterns = @statements.values.select(&:pattern?).each { yield it }
+    def get(key) = @statements[key]
+
+    def add(statement, code, title, filename)
+      c = code[0]
+      w = c == 'W'
+      @syntax.check(statement) unless statement[0] == '/' &&
+                                      statement[-1] == '/' &&
+                                      %w[A L M E I].include?(c)
+      restatement = @statements.values.detect do |restatement|
+        (w || restatement.type == c) && restatement.to_s == statement
       end
-      @heap.add restatement
-      code, = restatement.code
-      title ||= restatement.title
-      return code, title
+      if restatement
+        unless 'DXSPTCRH'.include?(restatement.type)
+          # Only allow heap-able statements to be restated.
+          raise Error, "restatement: #{restatement.code}"
+        end
+
+        @heap.add restatement
+        code, = restatement.code
+        title ||= restatement.title
+        return code, title
+      end
+      statement_number = yield
+      @last = Statement.new(statement, code, title, filename, statement_number,
+                            self)
+      raise Error, "duplicate key: #{@last.key}" if @statements.key?(@last.key)
+
+      @statements[@last.key] = @last
+      @symbols.define! @last if 'AIEMLDXS'.include?(@last.type)
+      @heap.add @last if 'DXSPTCRH'.include?(@last.type)
+      [@last.code, @last.title]
     end
-    statement_number = yield
-    @last=Statement.new(statement,code,title,filename,statement_number,self)
-    raise Error, "duplicate key: #{@last.key}" if @statements.key?(@last.key)
-    @statements[@last.key] = @last
-    @symbols.define! @last if 'AIEMLDXS'.include?(@last.type)
-    @heap.add @last if 'DXSPTCRH'.include?(@last.type)
-    [@last.code, @last.title]
   end
-end
 end
