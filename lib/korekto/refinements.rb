22296f97c05f427711bdd920e430d616c95fd597
CarlosJHR64 <carlosjhr64@gmail.com>
2025-12-16 07:41:47 -0800

    Reeking Refinements

--- lib/korekto/refinements.rb
+++ lib/korekto/refinements.rb
@@ -5,28 +5,29 @@ module Korekto
   module Refinements
     refine ::String do
       # Checks if brackets are balanced
+      # :reek:FeatureEnvy :reek:TooManyStatements
       def balanced?(brackets = '()[]{}')
         stack = []
-        chars.each do |c|
-          next unless brackets.include?(c)
+        chars.each do |char|
+          next unless brackets.include?(char)
 
-          q, r = brackets.index(c).divmod(2)
-          if r.zero? then stack << q
+          group, index = brackets.index(char).divmod(2)
+          if index.zero? then stack.push(group)
           else
-            stack.last == q ? stack.pop : stack << q
+            stack.last == group ? stack.pop : stack.push(group)
           end
         end
         stack.empty?
       end
 
       # True if none of the chars have space to their left.
-      def ltight?(*chars)  = chars.all? { |c| !include?(" #{c}") }
+      def ltight?(*chars) = chars.all? { |char| !include?(" #{char}") }
 
       # True if none of the chars have space to their right.
-      def rtight?(*chars)  = chars.all? { |c| !include?("#{c} ") }
+      def rtight?(*chars) = chars.all? { |char| !include?("#{char} ") }
 
       # True if all chars are both ltight and rtight.
-      def tight?(*chars)   = chars.all? { |c| ltight?(c) && rtight?(c) }
+      def tight?(*chars) = chars.all? { |char| ltight?(char) && rtight?(char) }
 
       def gsub_tokens!(tokens)
         # Reverse to replace higher-index captures ($10, $11, ...)
